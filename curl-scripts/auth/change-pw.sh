6f9dcd1b89ec4140a348b44f4312d2cffdcb8bb7
Hannah <hannah@Hannahs-MBP.attlocal.net>
2021-02-18 12:10:03 -0500

    commiting for deploy

--- curl-scripts/auth/change-pw.sh
+++ curl-scripts/auth/change-pw.sh
@@ -2,7 +2,7 @@
 curl "https://tic-tac-toe-api-development.herokuapp.com/change-pw" \
   --include \
   --request PATCH \
-  --header "Authorization: Bearer=${TOKEN}" \
+  --header "Authorization: Bearer ${TOKEN}" \
   --header "Content-Type: application/json" \
   --data '{
     "passwords": {
